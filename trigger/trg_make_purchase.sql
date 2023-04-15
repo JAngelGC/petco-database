138dc659c8109c7e3e76bfe25ac8e51fd3cd6955
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-15 14:21:42 -0600

    Creating trigger when a product reaches 0 in stock

---
+++ trigger/trg_make_purchase.sql
@@ -0,0 +1,18 @@
+DELIMITER //
+
+CREATE TRIGGER trg_make_purchase
+BEFORE UPDATE ON product 
+FOR EACH ROW
+BEGIN
+    -- make the insert to the purchase table
+    -- update stock
+    -- select arbitraries provider and manager
+    IF NEW.stock = 0 THEN
+        SET NEW.stock = 10;
+        INSERT INTO purchase(product_id, quantity, provider_id, manager_id, purchase_date, total_amount) VALUES
+        (OLD.product_id, 10, 'PR001', 'M0001', CURRENT_TIMESTAMP(), OLD.price*10);  
+    END IF;
+
+END //
+
+DELIMITER ;
