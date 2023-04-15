40a9668765c0ef68628198e0e2cbd988136b9f32
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-15 12:14:23 -0600

    Finished stored procedure to make a sale

--- sp/sp_make_sale.sql
+++ sp/sp_make_sale.sql
@@ -1,28 +1,50 @@
--- Write a stored procedure that implements the logic to make a sale.
--- receive the id of the product, the quantity and the id of the sale,
--- generate the appropriate record in the sales tables, and also work 
--- with a transaction that decrements the number of items available,
--- if the sale is not possible, rollback the transaction
--- (avoid negative numbers in quantity of items available).
-
---   sale_id VARCHAR(10) PRIMARY KEY,
---   product_id VARCHAR(10),
---   cashier_id VARCHAR(10),
---   customer_id VARCHAR(10),
---   sale_date DATE,
---   total_amount DECIMAL(10, 2),
-
--- call sp_make_sale('P0001', 'E0001', 'C0001', 2)
+-- CALL sp_make_sale('P0001', 'E0001', 'C0001', 10);
 -- DROP PROCEDURE sp_make_sale;
 DELIMITER //
 CREATE PROCEDURE sp_make_sale(IN product_id VARCHAR(10), IN cashier_id VARCHAR(10), IN customer_id VARCHAR(10), IN quantity INT)
 BEGIN
 
-    IF quantity < 0 THEN
-        SELECT 'Cannot accept negative quantities' as result;
-    ELSE
-        SELECT * FROM product WHERE product.product_id = product_id;
-    END IF;
+    DECLARE cur_quantity INT;
+    DECLARE new_quantity INT;
+    DECLARE price_product DECIMAL;
+    DECLARE cur_time TIMESTAMP;
+
+    START TRANSACTION;
+
+        -- check for negative quantities
+        IF quantity < 0 THEN
+            SELECT 'Cannot accept negative quantities' as result;
+            ROLLBACK;
+        ELSE
+            SELECT stock INTO cur_quantity FROM product WHERE product.product_id = product_id;
+            SET new_quantity = cur_quantity - quantity;
+    
+            IF new_quantity < 0 THEN
+                SELECT 'Cannot make the sale. Not enough stock' as result;
+                ROLLBACK;
+            ELSE
+                -- save product price and timestamp
+                SELECT price INTO price_product FROM product WHERE product.product_id = product_id;
+                SET cur_time = CURRENT_TIMESTAMP();
+
+                -- make the update to product stock
+                UPDATE product 
+                SET 
+                    stock = new_quantity
+                WHERE
+                    product.product_id = product_id;
+
+                -- make the sale
+                INSERT INTO sale (product_id, cashier_id, customer_id, sale_date, total_amount) VALUES
+                (product_id, cashier_id, customer_id, cur_time, price_product*quantity);
+
+                -- print sale
+                SELECT * from sale where sale.sale_date = cur_time;
+                COMMIT;
+
+            END IF;
+        END IF;
+
 
 
 END //
